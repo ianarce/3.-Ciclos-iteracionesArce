87c8ac42601daebad3275e01fd32c06f1b42bd23
ianarce <66857616+ianarce@users.noreply.github.com>
2021-12-22 20:49:13 -0600

    Add files via upload

---
+++ CLASE 3/main.js
@@ -0,0 +1,7 @@
+let texto = prompt("Ingresa el mensaje para mostrarlo en pantalla")
+
+let vecesRepetir= parseInt(prompt("Ingresa las veces a imprimir el mensaje"))
+
+for (let i = 1;i <=vecesRepetir;i++){
+    alert(texto)
+}
